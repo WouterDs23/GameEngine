af81a3d6aed3e6135b3cd4ba40a05ceda0936a9d
WouterHome <48447813+WouterDs23@users.noreply.github.com>
2019-05-17 11:16:56 +0200

    Added more states

--- Exam_Assignment/DigDug/CharacterBehaviour.cpp
+++ Exam_Assignment/DigDug/CharacterBehaviour.cpp
@@ -9,28 +9,65 @@ bool WalkingState::HandleInput(std::weak_ptr<dae::GameObject> obj, std::weak_ptr
 	{
 		std::string newState = typeid(input.lock()->sortCommand.operator*()).name();
 		std::string currentState = typeid(obj.lock()->GetState().lock().operator*()).name();
-		if (newState == "class MoveDown")
+		if (newState == "class MoveDown" || newState == "class MoveUp" || newState == "class MoveRight" || newState == "class MoveLeft")
 		{
 			return input.lock()->sortCommand->execute(obj);
 		}
-		if (newState == "class MoveUp")
+		if (newState == "class dae::ExitGame")
 		{
 			return input.lock()->sortCommand->execute(obj);
 		}
-		if (newState == "class MoveRight")
+		if (newState == "class Shoot")
 		{
+			obj.lock()->SetState(std::make_shared<ShootingState>());
 			return input.lock()->sortCommand->execute(obj);
 		}
-		if (newState == "class MoveLeft")
-		{
-			return input.lock()->sortCommand->execute(obj);
-		}
-		return input.lock()->sortCommand->execute(obj);
 	}
 	return false;
 }
 
-void WalkingState::Update(std::weak_ptr<dae::GameObject>)
+void WalkingState::Update(std::weak_ptr<dae::GameObject> obj)
+{
+	if (obj.lock())
+	{
+		
+	}	
+}
+
+bool ShootingState::HandleInput(std::weak_ptr<dae::GameObject> obj, std::weak_ptr<dae::Input> input)
+{
+	if (input.lock() && obj.lock())
+	{
+		std::string newState = typeid(input.lock()->sortCommand.operator*()).name();
+		std::string currentState = typeid(obj.lock()->GetState().lock().operator*()).name();
+		if (newState == "class MoveDown" || newState == "class MoveUp" || newState == "class MoveRight" || newState == "class MoveLeft")
+		{
+			obj.lock()->SetState(std::make_shared<WalkingState>());
+			return input.lock()->sortCommand->execute(obj);
+		}
+		if (newState == "class dae::ExitGame")
+		{
+			return input.lock()->sortCommand->execute(obj);
+		}
+		if (newState == "class Shoot")
+		{
+			return input.lock()->sortCommand->execute(obj);
+		}
+	}
+	return false;
+}
+
+void ShootingState::Update(std::weak_ptr<dae::GameObject>)
+{
+
+}
+
+bool HitState::HandleInput(std::weak_ptr<dae::GameObject> obj, std::weak_ptr<dae::Input> input)
+{
+	return false;
+}
+
+void HitState::Update(std::weak_ptr<dae::GameObject>)
 {
 
 }
--- Exam_Assignment/DigDug/CharacterBehaviour.h
+++ Exam_Assignment/DigDug/CharacterBehaviour.h
@@ -11,3 +11,22 @@ public:
 	void Update(std::weak_ptr<dae::GameObject>) override;
 };
 
+class ShootingState final : public dae::BaseState
+{
+public:
+	ShootingState() = default;
+	~ShootingState() override = default;
+
+	bool HandleInput(std::weak_ptr<dae::GameObject> obj, std::weak_ptr<dae::Input> input) override;
+	void Update(std::weak_ptr<dae::GameObject>) override;
+};
+
+class HitState final : public dae::BaseState
+{
+public:
+	HitState() = default;
+	~HitState() override = default;
+
+	bool HandleInput(std::weak_ptr<dae::GameObject> obj, std::weak_ptr<dae::Input> input) override;
+	void Update(std::weak_ptr<dae::GameObject>) override;
+};
